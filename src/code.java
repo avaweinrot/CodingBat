fe2438628c8f4bcb1b2231103987e5bf3b88d5f0
ava weinrot <avaweinrot@icloud.com>
2022-10-24 15:33:52 -0700

    coding bat (working)

---
+++ src/code.java
@@ -0,0 +1,13 @@
+
+public class code {
+
+	public int sum3(int[] nums) {
+		  int sum = 0;
+		  for (int k = 0; k < nums.length; k++)
+		  {
+		    sum += nums[k];
+		  }
+		  return sum;
+		}
+
+	}
